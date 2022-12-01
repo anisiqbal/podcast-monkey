44bad65ef86332209495f1b298204b94ef163146
Muhammad Awais <awaisshaikh94@gmail.com>
2022-12-01 12:16:43 +0500

    fontawesome icon added

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # podcast-monkey
+
+# Adding Bootstrap
+https://create-react-app.dev/docs/adding-bootstrap/
+
+# Font Awesome with React
+https://fontawesome.com/docs/web/use-with/react/
--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faMagnifyingGlassChart } from '@fortawesome/free-solid-svg-icons'
+
 function App() {
   return (
    <div>
     <h1>Project</h1>
     <a className='btn btn-primary'>Primary</a>
+
+      <FontAwesomeIcon icon={faEnvelope} />
+      <FontAwesomeIcon icon={faMagnifyingGlassChart} />
    </div>
   );
 }
